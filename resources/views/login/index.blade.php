6d6de897eab526df1d9534e870750a96e7c99817
ilhamgepe <gepedevelop@gmail.com>
2021-12-03 16:58:17 +0700

    test aja artibut login post

--- resources/views/login/index.blade.php
+++ resources/views/login/index.blade.php
@@ -8,15 +8,16 @@
                 <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
             </div>
         @endif
-        <form>
+        <form action="/login" method="POST">
+            @csrf
             <h1 class="h3 mb-3 fw-normal">Please log in</h1>
 
             <div class="form-floating">
-                <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
+                <input type="email" name="email" class="form-control" id="floatingInput" placeholder="name@example.com">
                 <label for="floatingInput">Email address</label>
             </div>
             <div class="form-floating">
-                <input type="password" class="form-control" id="floatingPassword" placeholder="Password">
+                <input type="password" name="password" class="form-control" id="floatingPassword" placeholder="Password">
                 <label for="floatingPassword">Password</label>
             </div>
 
